fce274bde68afcfc7715392d3b225e88bd036f9a
ihab <ihab@utopios.net>
2022-03-10 20:13:36 +0100

    upload file to storage

--- demo-api/demo-blob/Controllers/UploadController.cs
+++ demo-api/demo-blob/Controllers/UploadController.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using Azure.Identity;
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace demo_blob.Controllers
@@ -11,19 +12,22 @@ namespace demo_blob.Controllers
     public class UploadController : ControllerBase
     {
         [HttpPost]
-        public IActionResult Post()
+        public IActionResult Post(IFormFile image)
         {
             //Accéder au storage blob
             // BlobContainerClient containerClient = new BlobContainerClient(new Uri("https://utopiosstorage.blob.core.windows.net/images"),
             //     new DefaultAzureCredential());
             
             //Accéder au storage par la chaine de connexion.
-            BlobServiceClient blobServiceClient = new BlobServiceClient(@"");
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
-            //Avoir notre image en bytes
-            byte[] blobFileBytes = new byte[256];
-            using MemoryStream memoryStream = new MemoryStream(blobFileBytes);
-            containerClient.UploadBlob("nom_du_fichier", memoryStream);
+             BlobServiceClient blobServiceClient = new BlobServiceClient(@"");
+             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
+            // //Avoir notre image en bytes
+            // byte[] blobFileBytes = new byte[256];
+            // using MemoryStream memoryStream = new MemoryStream(blobFileBytes);
+            BlobClient blob = containerClient.GetBlobClient(image.FileName);
+            blob.Upload(image.OpenReadStream(), true);
+            
+            
             return Ok();
         }
         
