aae0c0ca0c7655b9b364b7166fb4776c3c42b082
ihab <ihab@utopios.net>
2022-03-10 19:52:50 +0100

    avec exemple de stockage par api de l'exterieur du cloud

--- demo-api/demo-blob/Controllers/UploadController.cs
+++ demo-api/demo-blob/Controllers/UploadController.cs
@@ -14,8 +14,12 @@ namespace demo_blob.Controllers
         public IActionResult Post()
         {
             //Accéder au storage blob
-            BlobContainerClient containerClient = new BlobContainerClient(new Uri("https://utopiosstorage.blob.core.windows.net/images"),
-                new DefaultAzureCredential());
+            // BlobContainerClient containerClient = new BlobContainerClient(new Uri("https://utopiosstorage.blob.core.windows.net/images"),
+            //     new DefaultAzureCredential());
+            
+            //Accéder au storage par la chaine de connexion.
+            BlobServiceClient blobServiceClient = new BlobServiceClient(@"");
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
             //Avoir notre image en bytes
             byte[] blobFileBytes = new byte[256];
             using MemoryStream memoryStream = new MemoryStream(blobFileBytes);
