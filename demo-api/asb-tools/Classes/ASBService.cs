6ec0c5e09ef63c3059a58f793f2a3ac9391556ea
ihab abadi <ihab@utopios.net>
2022-03-17 20:03:09 +0100

    received message

--- demo-api/asb-tools/Classes/ASBService.cs
+++ demo-api/asb-tools/Classes/ASBService.cs
@@ -1,4 +1,5 @@
 using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
 
 namespace asb_tools.Classes;
 
@@ -21,5 +22,29 @@ public class ASBService
     {
         _serviceBusClient = new ServiceBusClient(connectionString);
         return _serviceBusClient.CreateProcessor(queueName);
+    }
+
+    public async void CreateTopic(string topicName)
+    {
+        ServiceBusAdministrationClient serviceBusAdministrationClient = new ServiceBusAdministrationClient(connectionString);
+        await serviceBusAdministrationClient.CreateTopicAsync(topicName);
+    }
+
+    public async void CreateSubscrption(string topicName, string subscriptionName)
+    {
+        ServiceBusAdministrationClient serviceBusAdministrationClient = new ServiceBusAdministrationClient(connectionString);
+        serviceBusAdministrationClient.CreateSubscriptionAsync(
+            new CreateSubscriptionOptions(topicName, subscriptionName));
+    }
+
+    public async Task ReceiveMessageAsync(string subscriptionName, string topicName)
+    {
+        _serviceBusClient = new ServiceBusClient(connectionString);
+        ServiceBusReceiver receiver = _serviceBusClient.CreateReceiver(topicName, subscriptionName);
+        while (true)
+        {
+            ServiceBusReceivedMessage messageReceived =await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(5));
+            //Récupération du message
+        }
     }
 }
